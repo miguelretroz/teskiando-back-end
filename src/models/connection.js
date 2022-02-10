af8a5135ec8fc4656b0732c98a98f338207c7ee3
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-10 20:57:27 -0300

    feat: db connection

---
+++ src/models/connection.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require('mongodb');
+
+const { MONGO_DB_URL, DB_NAME } = process.env;
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = async () => {
+  try {
+    return db
+    || MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+
+      return db;
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connection;
