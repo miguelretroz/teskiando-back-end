cd070e99809d7ebfe7c62277cb5129443495c6be
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-11 12:38:54 -0300

    feat: error middleware

--- src/api/server.js
+++ src/api/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const app = require('./app');
 
+app.use(require('../errors/handle'));
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ src/errors/handle.js
@@ -0,0 +1,12 @@
+const rescue = require('express-rescue');
+const { UNPROCESSABLE_ENTITY } = require('http-status-codes').StatusCodes;
+
+module.exports = rescue(
+  (err, _req, res, _next) => {
+    console.error(err);
+
+    if (err.isJoi) return res.status(UNPROCESSABLE_ENTITY).json({ message: err.message });
+
+    res.status(500).json({ message: 'Internal Server Error' });
+  },
+);
