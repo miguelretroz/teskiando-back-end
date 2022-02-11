f790bb4cbff0f4b8cd2388eef00a36868f823ee1
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-11 18:15:52 -0300

    feat: implement cookie parser

--- src/api/app.js
+++ src/api/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const { COOKIE_SECRET } = process.env;
+
 app.use(express.json());
+app.use(require('cookie-parser')(COOKIE_SECRET));
 
 app.use(require('../controllers/root'));
 
