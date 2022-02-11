725aaee11a7fad745ca53447cc06af51d9542186
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-11 15:42:24 -0300

    feat: add new custom messages for user registration validation

--- src/errors/defaultMessages.js
+++ src/errors/defaultMessages.js
@@ -1,1 +1,8 @@
 module.exports.ALREADY_REGISTERED = (key) => `${key} já registrado`;
+module.exports.INCORRECT_TYPE = (key, type) => `${key} deve ser "${type}"`;
+module.exports.REQUIRED = (key) => `${key} é obrigatório`;
+module.exports.INVALID_PATTERN = (key) => `${key} no formato incorreto`;
+module.exports.MIN_LENGTH = (key, min) => `${key} deve ter ${min} ou mais caracteres`;
+module.exports.PASSWORDS_MUST_BE_EQUAL = () => 'As senhas devem ser iguais';
+module.exports.NOT_FOUND = (key) => `${key} não encontrado`;
+module.exports.INCORRECT = (key) => `${key} incorret${key[key.length - 1] === 'a' ? 'a' : 'o'}`;
--- src/validations/user/create.js
+++ src/validations/user/create.js
@@ -1,9 +1,24 @@
 const Joi = require('joi');
 
 const { regex } = require('../../helpers');
+const { defaultMessages } = require('../../errors');
+
+const NAME_MIN_LENGTH = 3;
 
 module.exports = Joi.object({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().pattern(regex.email).required(),
-  password: Joi.string().pattern(regex.password).required(),
+  name: Joi.string().min(NAME_MIN_LENGTH).required().messages({
+    'string.base': defaultMessages.INCORRECT_TYPE('Nome', 'string'),
+    'string.empty': defaultMessages.REQUIRED('Nome'),
+    'string.min': defaultMessages.MIN_LENGTH('Nome', NAME_MIN_LENGTH),
+  }),
+  email: Joi.string().pattern(regex.email).required().messages({
+    'string.base': defaultMessages.INCORRECT_TYPE('Email', 'string'),
+    'string.empty': defaultMessages.REQUIRED('Email'),
+    'string.pattern.base': defaultMessages.INVALID_PATTERN('Email'),
+  }),
+  password: Joi.string().pattern(regex.password).required().messages({
+    'string.base': defaultMessages.INCORRECT_TYPE('Senha', 'string'),
+    'string.empty': defaultMessages.REQUIRED('Senha'),
+    'string.pattern.base': defaultMessages.INVALID_PATTERN('Senha'),
+  }),
 });
