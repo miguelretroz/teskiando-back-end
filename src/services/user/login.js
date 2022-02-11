6f207b485f53ee91de878aab794154397f1b2c91
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-11 20:22:15 -0300

    feat: generates `access token` at the user login

--- src/services/user/login.js
+++ src/services/user/login.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { userModels, refreshTokenModels } = require('../../models');
 const { apiError, creators } = require('../../errors');
+
+const { JWT_SECRET_KEY } = process.env;
 
 module.exports = async ({ email, password }) => {
   const user = await userModels.getByEmail(email);
@@ -12,9 +15,11 @@ module.exports = async ({ email, password }) => {
 
   if (!passwordIsCorrect) return apiError(creators.incorrect('Senha'));
 
-  const { _id } = user;
+  const { _id, name } = user;
 
   const refreshToken = await refreshTokenModels.create(_id);
 
-  return { refreshToken, token: 'token' };
+  const token = jwt.sign({ sub: _id, name, email }, JWT_SECRET_KEY, { expiresIn: 15 * 60 });
+
+  return { refreshToken, token };
 };
