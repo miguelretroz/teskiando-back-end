22c3271b76d89814e420e0404634643f86de1cf9
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-14 19:52:04 -0300

    fix: reatribution condition

--- src/services/task/update.js
+++ src/services/task/update.js
@@ -24,8 +24,8 @@ module.exports = async (userId, taskId, { title, status }) => {
 
   const newData = { title: task.title, status: task.status };
 
-  if (title !== undefined || title !== '') newData.title = title;
-  if (status !== undefined || status !== '') newData.status = status;
+  if (title !== undefined && title !== '') newData.title = title;
+  if (status !== undefined && status !== '') newData.status = status;
 
   await taskModels.update(taskId, newData);
 };
