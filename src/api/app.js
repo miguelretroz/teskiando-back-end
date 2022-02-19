e4c30648aee5f02a129a765e565550e79dd692a4
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-02-18 22:54:44 -0300

    refactor: move origins assignment from cors to env variable

--- src/api/app.js
+++ src/api/app.js
@@ -4,17 +4,15 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const { COOKIE_SECRET } = process.env;
+const { COOKIE_SECRET, ORIGINS_LIST } = process.env;
 
 app.use(express.json());
 app.use(cookieParser(COOKIE_SECRET));
 
+const origin = ORIGINS_LIST ? ORIGINS_LIST.split('\n') : [];
+
 const corsConfig = {
-  origin: [
-    'http://localhost:3000',
-    'https://teskiando.vercel.app',
-    'https://teskiando-miguelretroz.vercel.app',
-  ],
+  origin,
   credentials: true,
 };
 
