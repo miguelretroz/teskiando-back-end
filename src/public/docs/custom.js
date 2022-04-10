b1ef0d2a5da89f5521a02b81a25b5aebe2f7b28c
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-10 19:32:33 -0300

    docs: moves add of meta data out of setTimeout'

--- src/public/docs/custom.js
+++ src/public/docs/custom.js
@@ -13,50 +13,50 @@ if (link16) {
   link16.href = '/docs/favicon-16x16.png';
 }
 
-setTimeout(() => {
-  const head = document.querySelector('head');
-  head.innerHTML += `
-    <meta
-    name="description"
+const head = document.querySelector('head');
+head.innerHTML += `
+  <meta
+  name="description"
+  content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida."
+  />
+  <meta name="author" content="Miguel Retroz" />
+  <meta
+    property="og:url"
+    content="https://teskiando-api.herokuapp.com"
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Téskiando" />
+  <meta
+    property="og:description"
     content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida."
-    />
-    <meta name="author" content="Miguel Retroz" />
-    <meta
-      property="og:url"
-      content="https://teskiando-api.herokuapp.com"
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Téskiando" />
-    <meta
-      property="og:description"
-      content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida."
-    />
-    <meta property="og:image" content="https://i.ibb.co/4f9VYRn/T-skiando-Banner.png" />
-    <meta
-      property="canonical"
-      content="https://teskiando-api.herokuapp.com"
-    />
-    <meta property="keywords" content="api, task, tasks, organization" />
-    <meta property="robots" content="api, task, tasks, organization" />
+  />
+  <meta property="og:image" content="https://i.ibb.co/4f9VYRn/T-skiando-Banner.png" />
+  <meta
+    property="canonical"
+    content="https://teskiando-api.herokuapp.com"
+  />
+  <meta property="keywords" content="api, task, tasks, organization" />
+  <meta property="robots" content="api, task, tasks, organization" />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta
-      property="twitter:domain"
-      content="teskiando-api.herokuapp.com"
-    />
-    <meta
-      property="twitter:url"
-      content="https://teskiando-api.herokuapp.com"
-    />
-    <meta name="twitter:title" content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida." />
-    <meta
-      name="twitter:description"
-      content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida."
-    />
-    <meta name="twitter:image" content="https://i.ibb.co/4f9VYRn/T-skiando-Banner.png" />
-    <meta name="theme-color" content="#3051FF" />
-  `;
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta
+    property="twitter:domain"
+    content="teskiando-api.herokuapp.com"
+  />
+  <meta
+    property="twitter:url"
+    content="https://teskiando-api.herokuapp.com"
+  />
+  <meta name="twitter:title" content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida." />
+  <meta
+    name="twitter:description"
+    content="Api do app Téskiando, que tem como objetivo ajudar usuários a gerenciar tarefas de maneira fácil e rápida."
+  />
+  <meta name="twitter:image" content="https://i.ibb.co/4f9VYRn/T-skiando-Banner.png" />
+  <meta name="theme-color" content="#3051FF" />
+`;
 
+setTimeout(() => {
   const topBar = document.querySelector('.topbar');
   topBar.style.backgroundColor = '#3051FF';
 
